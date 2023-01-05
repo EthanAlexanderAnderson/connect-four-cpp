077f3f08eb72a7a107acf3a9f95187a913a8aa2c
Ethan Anderson <et714492@dal.ca>
2023-01-05 15:32:08 -0400

    Added Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+// Singleplayer Connect-four in C++
+// By Ethan Anderson
+
+#include <stdlib.h>
+#include <iostream>
+#include <ctime>
+
+int main() {
+    // Startup and welcome
+    std::cout << "Hello welcome to connect-four.\n";
+}
