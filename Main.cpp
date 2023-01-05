f071c6bc3b2c94b3e5db40a785ef2c14de98d83a
Ethan Anderson <et714492@dal.ca>
2023-01-05 16:26:10 -0400

    Create and print empty board

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,31 @@
 #include <iostream>
 #include <ctime>
 
+void printBoard(char board[][7]) {
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 7; j++){
+            std::cout << board[i][j];
+        }
+        std::cout << "\n";
+    }
+}
+
 int main() {
+
+    // Initialize variables
+    char board [7][7]; // [row] [column]
+
+    // Fill board with empty symbols
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 7; j++){
+            board[i][j] = '_';
+        }
+    }
+
     // Startup and welcome
     std::cout << "Hello welcome to connect-four.\n";
+
+    printBoard(board);
+
+    return 0;
 }
